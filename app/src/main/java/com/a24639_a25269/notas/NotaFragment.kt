b10d97ec314a27471bcaed63e991fbe7db166468
Cristiane Mayabanza <152671012+itscristianee@users.noreply.github.com>
2025-01-12 15:59:44 +0000

    Estrutura de navegação com BackStack

--- app/src/main/java/com/a24639_a25269/notas/NotaFragment.kt
+++ app/src/main/java/com/a24639_a25269/notas/NotaFragment.kt
@@ -6,17 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-
-class LoginFragment : Fragment() {
-
+class NotaFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        return inflater.inflate(R.layout.fragment_nota, container, false)
     }
-
 
 }
